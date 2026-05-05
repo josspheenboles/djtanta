512281e77b00aea207f615089ae80604cc6f519b
josspheenboles <eng.jospheenboles@gmail.com>
2026-05-05 10:57:45 +0300

    first model and migrations

--- catagory/models.py
+++ catagory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Catagory(models.Model):
+    id=models.IntegerField(primary_key=True,verbose_name='ID',help_text='catagory id')
+    name=models.CharField(max_length=100,help_text='catagory name',verbose_name='Catagory  Name')
+    # test=models.TextField()
+
+
+
