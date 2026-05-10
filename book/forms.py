ce600d84dc887a39ebd617d6561759c8f989aec2
josspheenboles <eng.jospheenboles@gmail.com>
2026-05-10 09:00:48 +0300

    forms- forign key in forms

--- book/forms.py
+++ book/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from catagory.models import Catagory
 class BookForm(forms.Form):
     title =forms.CharField(max_length=100,required=True)
     description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
     price = forms.DecimalField(max_digits=10,decimal_places=2)
     image = forms.ImageField()
-    # Catagory=forms.ChoiceField(choices=)
+    Catagory=forms.ChoiceField(choices=[(cat.id,cat.name) for cat in Catagory.objects.all()])
